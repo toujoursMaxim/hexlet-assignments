a9f05986b69a8f9358855907de1225d60a418e68
@hexlet/cli <support@hexlet.io>
2023-05-13 16:25:58 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -7,27 +7,27 @@ import java.util.Map.Entry;
 
 // BEGIN
 class App {
-public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map <String, String> serchBooks) {
-    List<Map<String, String>> result = new ArrayList<>();
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> serchBooks) {
+        List<Map<String, String>> result = new ArrayList<>();
 
-    for (Map<String, String> book : books) {
-        boolean test = false;
-        for (Map.Entry<String, String> serchBooks : serchBooks.entrySet()) {
-            if (book.containsKey(serchBooks.getKey())) {
-                if (book.containsValue(serchBooks.getValue())) {
-                    test = true;
-                } else {
-                    test = false;
-                    break;
+        for (Map<String, String> book : books) {
+            boolean test = false;
+            for (Map.Entry<String, String> serchBooks : serchBooks.entrySet()) {
+                if (book.containsKey(serchBooks.getKey())) {
+                    if (book.containsValue(serchBooks.getValue())) {
+                        test = true;
+                    } else {
+                        test = false;
+                        break;
+                    }
                 }
             }
-        }
-        if (test) {
-            result.add(book);
+            if (test) {
+                result.add(book);
             }
         }
 
-    return result;
+        return result;
     }
 }
 //END

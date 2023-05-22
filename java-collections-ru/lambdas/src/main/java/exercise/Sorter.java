1f49c27a204cb40935317be7463fb59257c6351b
@hexlet/cli <support@hexlet.io>
2023-05-22 17:13:23 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -3,7 +3,6 @@ package exercise;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.List;
-import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 // BEGIN
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -1,7 +1,7 @@
 package exercise;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
 class AppTest {
